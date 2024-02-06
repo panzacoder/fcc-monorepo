3df0d9f3ea9d2970ecd7b23bb7e14ddbfa231861
Jake Hebert <jacobshebert@gmail.com>
2024-02-06 12:58:00 -0600

    remove comments that came back

--- apps/expo/app/(authenticated)/_layout.tsx
+++ apps/expo/app/(authenticated)/_layout.tsx
@@ -2,39 +2,15 @@ import { Tabs } from 'expo-router/tabs'
 
 import MyTabBar from 'app/ui/tab-bar'
 
-// TODO: figure out why we get a soft warning about Reanimated. Likely to do with nativewind v4
-// I have double checked that the same Reanimated version is being used in expo sdk v50 and my own code.
-
 export default function Root() {
   return (
     <Tabs
       tabBar={MyTabBar}
       screenOptions={{ headerShown: false, tabBarShowLabel: false }}
     >
-      <Tabs.Screen
-        name="home"
-        // options={{
-        //   tabBarIcon: ({ color, size }) => (
-        //     <Feather name="home" size={size} color={color} />
-        //   ),
-        // }}
-      />
-      <Tabs.Screen
-        name="circles"
-        // options={{
-        //   tabBarIcon: ({ color, size }) => (
-        //     <Feather name="circle" size={size} color={color} />
-        //   ),
-        // }}
-      />
-      <Tabs.Screen
-        name="planner"
-        // options={{
-        //   tabBarIcon: ({ color, size }) => (
-        //     <Feather name="calendar" size={size} color={color} />
-        //   ),
-        // }}
-      />
+      <Tabs.Screen name="home" />
+      <Tabs.Screen name="circles" />
+      <Tabs.Screen name="planner" />
     </Tabs>
   )
 }
