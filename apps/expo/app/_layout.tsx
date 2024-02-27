15b7059311559a292aa15e56467c0351200b3d08
Jake Hebert <jacobshebert@gmail.com>
2024-02-27 12:27:51 -0600

    bump deps, remove logbox workaround for reanimated

--- apps/expo/app/_layout.tsx
+++ apps/expo/app/_layout.tsx
@@ -1,12 +1,7 @@
 import 'app/config/tailwind/global.css'
 import { Slot } from 'expo-router'
-import { LogBox } from 'react-native'
 
 import { Provider } from 'app/provider'
-
-// TODO: figure out why we get a soft warning about Reanimated. Likely to do with nativewind v4
-// I have double checked that the same Reanimated version is being used in expo sdk v50 and my own code.
-LogBox.ignoreLogs(['[Reanimated]'])
 
 export default function Root() {
   return (
