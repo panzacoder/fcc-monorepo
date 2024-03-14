402c11dcaa2d1e7f694c92a9f6d5319d0bb74dc5
Jake Hebert <jacobshebert@gmail.com>
2024-03-13 20:33:56 -0500

    remove unused headers

--- apps/link-tracking/middleware.ts
+++ apps/link-tracking/middleware.ts
@@ -4,8 +4,6 @@ import type { NextRequest } from 'next/server'
 const originDomain = 'https://track.customer.io'
 export function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers)
-  // requestHeaders.set('X-Forwarded-Proto', 'https')
-  // requestHeaders.set('X-Forwarded-Host', 'track.customer.io')
   requestHeaders.set('Host', 'email.familycarecircle.app')
 
   const rewritePath = new URL(request.nextUrl.pathname, originDomain)
