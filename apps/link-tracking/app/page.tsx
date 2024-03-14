6a4fa0fa75406ac14a397d536dfcb7a736fae0e6
Jake Hebert <jacobshebert@gmail.com>
2024-03-13 19:06:12 -0500

    add home page to make build happy

---
+++ apps/link-tracking/app/page.tsx
@@ -0,0 +1,12 @@
+export default function RootPage() {
+  return (
+    <main>
+      <h1>Family Care Circle</h1>
+      <p>This domain is for link tracking in our marketing campaigns.</p>
+      <p>If you landed here, something probably went wrong. </p>
+      <a href="https://familycarecircle.app">
+        Head on over to our home page to get back to our product
+      </a>
+    </main>
+  )
+}
