4130bcd0165a146ee20e891ac8df0580734d75a0
Jake Hebert <jacobshebert@gmail.com>
2024-03-13 19:21:37 -0500

    add matcher and remove headers

--- apps/link-tracking/middleware.ts
+++ apps/link-tracking/middleware.ts
@@ -1,14 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 const originDomain = 'https://track.customer.io'
-export async function middleware(request: NextRequest) {
+export function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers)
-  requestHeaders.set('X-Forwarded-Proto', 'https')
-  requestHeaders.set('X-Forwarded-Host', 'track.customer.io')
+  // requestHeaders.set('X-Forwarded-Proto', 'https')
+  // requestHeaders.set('X-Forwarded-Host', 'track.customer.io')
   requestHeaders.set('Host', 'email.familycarecircle.app')
 
   const rewritePath = new URL(request.nextUrl.pathname, originDomain)
   return NextResponse.rewrite(rewritePath, {
     headers: requestHeaders
   })
+}
+
+export const config = {
+  matcher: '/*'
 }
