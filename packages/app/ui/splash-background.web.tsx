c21e0d79915f4dc9bda07a818e5b152d128e2880
Jake Hebert <jacobshebert@gmail.com>
2024-04-04 17:47:10 -0500

    fix z-index issue with opacity filter

--- packages/app/ui/splash-background.web.tsx
+++ packages/app/ui/splash-background.web.tsx
@@ -3,8 +3,8 @@
 export function SplashBackground({ children }) {
   return (
     <div className="web-splash-gradient flex h-screen w-screen">
-      <div className="absolute h-screen w-screen bg-black opacity-30"></div>
-      <div className="flex flex-1 items-center justify-center px-4">
+      <div className="absolute z-0 h-screen w-screen bg-black opacity-30"></div>
+      <div className="z-10 flex flex-1 items-center justify-center px-4">
         {children}
       </div>
     </div>
