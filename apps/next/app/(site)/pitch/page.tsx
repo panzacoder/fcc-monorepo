29599e6cd7d6f5e1c809ca794ef9bbd747d97102
Jake Hebert <jacobshebert@gmail.com>
2024-04-11 18:46:49 -0500

    iframe embed

---
+++ apps/next/app/(site)/pitch/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+export default function PitchPage() {
+  return (
+    <div className="w-full bg-transparent">
+      <div className="relative mb-5 mt-8 h-0 w-full overflow-hidden rounded-2xl bg-transparent pb-[56.25%] shadow-md">
+        <iframe
+          loading="lazy"
+          className="padding-0 margin-0 absolute left-0 top-0 h-full w-full border-none"
+          src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGBZlJxv1U&#x2F;4FRIFrPTqJe_NvWxjFPqgw&#x2F;view?embed"
+          allowFullScreen
+          allow="fullscreen"
+        ></iframe>
+      </div>
+      <a
+        href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGBZlJxv1U&#x2F;4FRIFrPTqJe_NvWxjFPqgw&#x2F;view?utm_content=DAGBZlJxv1U&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link"
+        target="_blank"
+        rel="noopener"
+      >
+        FCC Pitch Deck
+      </a>
+    </div>
+  )
+}
