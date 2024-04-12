3db6d04b79152d6d048da335264d41a1ef14be3e
Jake Hebert <jake@plfx.studio>
2024-04-12 11:10:54 -0500

    BUG: responsive design cleanup (#58)
    responsive design cleanup
    
    - fix broken grid-rows setting on (site) layout group
    - remove black transparent bg on pitch page for simplicity
    - remove unecessary margin on pitch page

--- apps/next/app/(site)/pitch/page.tsx
+++ apps/next/app/(site)/pitch/page.tsx
@@ -4,22 +4,22 @@ import { Typography } from 'app/ui/typography'
 
 export default function PitchPage() {
   return (
-    <div className="absolute grid h-screen w-screen place-items-center bg-black/40 p-10">
+    <div className="grid  justify-items-center gap-3 px-10">
       <Typography
         variant="h1"
         className="text-primary-foreground drop-shadow-lg"
       >
         Interested in investing?
       </Typography>
-      <div className="grid w-full place-items-center">
+      <div className="grid w-full justify-items-center gap-2">
         <Typography
           variant="h3"
-          className="text-primary-foreground drop-shadow-lg"
+          className="text-primary-foreground h-min drop-shadow-lg"
         >
           Check out our plans for the future 👇
         </Typography>
 
-        <div className="relative mb-5 mt-8 h-0 w-full overflow-hidden rounded-2xl  bg-transparent pb-[56.25%] shadow-md">
+        <div className="relative h-0 w-full max-w-screen-xl overflow-hidden rounded-2xl pb-[56.25%] shadow-md">
           <iframe
             loading="lazy"
             className="padding-0 margin-0 absolute left-0 top-0 h-full w-full border-none"
