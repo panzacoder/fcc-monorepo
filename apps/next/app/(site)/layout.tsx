4f534b10f87b05a0f05123ee1b8d18f9ba685b9f
Jake Hebert <jacobshebert@gmail.com>
2024-04-12 10:42:41 -0500

    responsive design cleanup
    - fix broken grid-rows setting on (site) layout group
    - remove black transparent bg on pitch page for simplicity
    - remove unecessary margin on pitch page

--- apps/next/app/(site)/layout.tsx
+++ apps/next/app/(site)/layout.tsx
@@ -6,7 +6,7 @@ import { AppBar } from './app-bar'
 export default function SplashLayout({ children }) {
   return (
     <SplashBackground>
-      <div className="grid-rows-[min-content 1fr] grid h-full w-full max-w-screen-xl grid-cols-1">
+      <div className="grid h-full w-full max-w-screen-xl grid-cols-1 grid-rows-[min-content_1fr]">
         <AppBar />
         {children}
       </div>
