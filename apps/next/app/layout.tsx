ffbfdfa35f1ff00384c3ed86e6dec77313132697
Jake Hebert <jacobshebert@gmail.com>
2024-05-10 16:09:28 -0500

    add site meta tags

--- apps/next/app/layout.tsx
+++ apps/next/app/layout.tsx
@@ -1,5 +1,7 @@
 /** @jsxImportSource react */
 
+import type { Metadata, Viewport } from 'next'
+
 import 'raf/polyfill'
 import 'setimmediate'
 import 'app/config/tailwind/global.css'
@@ -7,6 +9,55 @@ import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 
 import { Provider } from 'app/provider'
+
+const APP_NAME = 'Family Care Circle'
+const APP_DEFAULT_TITLE = 'Family Care Circle'
+const APP_TITLE_TEMPLATE = '%s - Family Care Circle'
+const APP_DESCRIPTION = 'The app for caregivers.'
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE
+  },
+  description: APP_DESCRIPTION,
+  // appleWebApp: {
+  //   // capable: true,
+  //   // statusBarStyle: 'black-translucent',
+  //   // title: APP_DEFAULT_TITLE,
+  //   // startupImage: [
+  //   //   '/splash_screens/10.2__iPad_landscape.png',
+  //   //   '/splash_screens/10.2__iPad_portrait.png',
+  //   //   '/splash_screens/10.5__iPad_landscape.png',
+  //   //   '/splash_screens/11__iPad_landscape.png'
+  //   // ]
+  // },
+  formatDetection: {
+    telephone: true
+  },
+  openGraph: {
+    type: 'website',
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE
+    },
+    description: APP_DESCRIPTION
+  },
+  twitter: {
+    card: 'summary',
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE
+    },
+    description: APP_DESCRIPTION
+  }
+}
+
+export const viewport: Viewport = {
+  themeColor: '#287CFA'
+}
 export default function RootLayout({
   children
 }: {
