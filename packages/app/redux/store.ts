5c5ad8ab00de106fa89fcc64f97d28e86e7e960a
shubham2795 <shubham.c@prototsolutions.com>
2024-07-02 20:45:47 +0530

    FCM integration changes

--- packages/app/redux/store.ts
+++ packages/app/redux/store.ts
@@ -1,20 +1,15 @@
-import { legacy_createStore as createStore } from 'redux'
-import rootReducer from './rootReducer'
-import StateLoader from './stateLoader'
+import {createStore} from 'redux';
+import rootReducer from './rootReducer';
+import StateLoader from './stateLoader';
 
-import { composeWithDevTools } from 'redux-devtools-extension'
-
-const stateLoader = new StateLoader()
+const stateLoader = new StateLoader();
 
 const store = createStore(
   rootReducer,
   stateLoader.loadState(),
-  composeWithDevTools()
-)
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 store.subscribe(() => {
-  stateLoader.saveState(store.getState())
-})
-export default store
-
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+  stateLoader.saveState(store.getState());
+});
+export default store;
