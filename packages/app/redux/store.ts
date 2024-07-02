a26c0c820dc3bfcee25d337d080d0f2ed0c39153
Jake Hebert <jacobshebert@gmail.com>
2024-07-02 11:18:46 -0500

    fix window undefined error in web ssr

--- packages/app/redux/store.ts
+++ packages/app/redux/store.ts
@@ -1,15 +1,21 @@
-import {createStore} from 'redux';
-import rootReducer from './rootReducer';
-import StateLoader from './stateLoader';
+import { createStore, compose } from 'redux'
+import rootReducer from './rootReducer'
+import StateLoader from './stateLoader'
 
-const stateLoader = new StateLoader();
+const stateLoader = new StateLoader()
 
-const store = createStore(
-  rootReducer,
-  stateLoader.loadState(),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION__?: typeof compose
+  }
+}
+const enhancers = compose(
+  typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : (f: any) => f
+)
+const store = createStore(rootReducer, stateLoader.loadState(), enhancers)
 store.subscribe(() => {
-  stateLoader.saveState(store.getState());
-});
-export default store;
+  stateLoader.saveState(store.getState())
+})
+export default store
