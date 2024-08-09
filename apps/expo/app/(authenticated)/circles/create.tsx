124d77bf39814c5b4ab818e757be8f591801ee8d
Jake Hebert <jacobshebert@gmail.com>
2024-08-09 14:19:30 -0500

    remove unused code

--- apps/expo/app/(authenticated)/circles/create.tsx
+++ apps/expo/app/(authenticated)/circles/create.tsx
@@ -1,7 +1,5 @@
-import { CreateCircleScreen } from 'app/features/createCircle/screen'
 import { CreateCircle } from 'app/features/circles/create/modal'
 
 export default function CreateCircleModal() {
-  // return <CreateCircleScreen />
   return <CreateCircle />
 }
